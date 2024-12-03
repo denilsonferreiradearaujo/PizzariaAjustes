f6e8a4dbf742b6a6ebd10cc20fb5b092a1a73ffb
Gabriel Pantalhão <gabriel.l.pantalhao@gmail.com>
2024-12-02 21:43:19 -0300

    Criando nova página de Taxa de entrega, falta resolver erro 404 get nela

--- backend/src/controllers/taxaEntrega/ListAllTaxaEntregaController.ts
+++ backend/src/controllers/taxaEntrega/ListAllTaxaEntregaController.ts
@@ -1,20 +1,21 @@
-import { Request, Response } from 'express'; 
-import { ListAllTaxaEntregaService } from '../../services/taxaEntrega/ListAllTaxaEntregaService';
+import { Request, Response } from "express";
+import { ListAllTaxaEntregaService } from "../../services/taxaEntrega/ListAllTaxaEntregaService";
 
 class ListAllTaxaEntregaController {
-    async handle(req: Request, res: Response) {
-        const listAllTaxaEntregaService = new ListAllTaxaEntregaService();
+  async handle(req: Request, res: Response) {
+    console.log("Rota /taxasEntrega foi chamada");
+    const listAllTaxaEntregaService = new ListAllTaxaEntregaService();
 
-        try {
-            const taxaEntrega = await listAllTaxaEntregaService.execute();
-            return res.json(taxaEntrega);
-        } catch (error) {
-            // Retorna um erro 500 em caso de falha
-            return res.status(500).json({
-                message: error.message || 'Erro inesperado ao listar taxas de entrega.',
-            });
-        }
+    try {
+      const taxaEntrega = await listAllTaxaEntregaService.execute();
+      console.log("Resposta enviada:", taxaEntrega);
+      return res.json(taxaEntrega);
+    } catch (error) {
+      console.error("Erro ao listar taxas de entrega:", error.message);
+      return res.status(500).json({
+        message: error.message || "Erro inesperado ao listar taxas de entrega.",
+      });
     }
+  }
 }
-
 export { ListAllTaxaEntregaController };
