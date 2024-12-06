40b3028a935494e577bf9fce559eb90f9335c6a6
George Samuel <126299897+GeorgeSamuel92@users.noreply.github.com>
2024-12-06 19:52:31 -0300

    copiado para maquina

---
+++ backend/prisma/migrations/20241206224811_pizzaria/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `pedido` MODIFY `numMesa` INTEGER NULL;
--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // baseURL: 'http://localhost:3333'
-    baseURL: 'http://10.0.3.122:3333'
+    baseURL: 'http://10.0.3.228:3333'
 })
 
 export {api};
